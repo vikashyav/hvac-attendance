29ee015a89222590e5a9eb7e54eb14bc06ba92c0
vikash1yadav <64723750+vikash1yadav@users.noreply.github.com>
2025-08-15 01:42:23 +0530

    added toast for checking

--- hooks/notification-sub-hook.js
+++ hooks/notification-sub-hook.js
@@ -35,54 +35,70 @@ export function useNotificationSubscrption() {
 
     const subscribeForPush = async (userId) => {
         // alert("1")
+        toast({
+            title: "111",
+            description: "Subscribed For Notification update",
+        })
         // try {
-            if (Notification.permission === "denied") {
-                alert("Notifications are blocked. Please enable them in browser settings.");
-                return;
-            }
+        if (Notification.permission === "denied") {
+            alert("Notifications are blocked. Please enable them in browser settings.");
+            return;
+        }
 
-            const permission = await Notification.requestPermission();
+        const permission = await Notification.requestPermission();
         // alert("2")
-            if (permission !== "granted") {
-                alert("Notifications permission not granted.");
-                return;
-            }
-            await navigator.serviceWorker.register('/sw.js')
-            // await navigator.serviceWorker.register('/sw.js')
-            const reg = await navigator.serviceWorker.ready;
+        toast({
+            title: "222",
+            description: "Subscribed For Notification update",
+        })
+        if (permission !== "granted") {
+            alert("Notifications permission not granted.");
+            return;
+        }
+        await navigator.serviceWorker.register('/sw.js')
+        // await navigator.serviceWorker.register('/sw.js')
+        const reg = await navigator.serviceWorker.ready;
         // alert("3")
+        toast({
+            title: "333",
+            description: "Subscribed For Notification update",
+        })
 
-            const sub = await reg.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: "BAc-mxt5YMzEkGC5aF1dUQ5n0pL_y51IzdO5jXOoaGSjSXcd5OhWBd05sRb28njnF2xORneihyZoHB7cm4BS_VQ"
-            });
-            if (sub) {
-                return saveSubscrptionMutation.mutate({ userId, subscription: sub }, {
-                    onSuccess: (res) => {
-                        toast({
-                            title: "Success",
-                            description: "Subscribed For Notification update",
-                        })
-                        console.log("✅ Push subscribed:", sub);
-                        setIsSubscribed(true);
+        const sub = await reg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: "BAc-mxt5YMzEkGC5aF1dUQ5n0pL_y51IzdO5jXOoaGSjSXcd5OhWBd05sRb28njnF2xORneihyZoHB7cm4BS_VQ"
+        });
+        toast({
+            title: "444",
+            description: "Subscribed For Notification update",
+        })
+        if (sub) {
+            return saveSubscrptionMutation.mutate({ userId, subscription: sub }, {
+                onSuccess: (res) => {
+                    toast({
+                        title: "Success",
+                        description: "Subscribed For Notification update",
+                    })
+                    console.log("✅ Push subscribed:", sub);
+                    setIsSubscribed(true);
+                    window.location.reload(); // to trigger middleware check
+                },
+                onError: () => {
+                    toast({
+                        title: "Error",
+                        description: "failed Subscribed For Notification update, Please contact support team",
+                        variant: "destructive",
+                    })
+                    alert("something went wrong")
+                    setTimeout(() => {
                         window.location.reload(); // to trigger middleware check
-                    },
-                    onError: () => {
-                        toast({
-                            title: "Error",
-                            description: "failed Subscribed For Notification update, Please contact support team",
-                            variant: "destructive",
-                        })
-                        alert("something went wrong")
-                        setTimeout(() => {
-                            window.location.reload(); // to trigger middleware check
-                        }, 100);
-                    }
-                })
-            }
+                    }, 100);
+                }
+            })
+        }
         // } catch (error) {
-            // console.error("❌ Push subscription failed:", err);
-            // alert("❌ Push subscription failed:", err)
+        // console.error("❌ Push subscription failed:", err);
+        // alert("❌ Push subscription failed:", err)
         // }
 
         // const registration = await navigator.serviceWorker.register('/sw.js').catch((err) => {
